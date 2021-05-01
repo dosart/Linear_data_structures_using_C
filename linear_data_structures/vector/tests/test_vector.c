416064113773886a468d6d3c59d6f9147ed659d2
Denis <Dysprosium92@ya.ru>
2021-05-01 21:11:12 +0300

    Added file test_vector.h

--- linear_data_structures/vector/tests/test_vector.c
+++ linear_data_structures/vector/tests/test_vector.c
@@ -1,36 +1,12 @@
 //
 // Created by dosart on 25.04.2021.
 //
-#include "../include/vector.h"
+
 #include <assert.h>
 #include <stdio.h>
 
-void test_vector_init_zero_size();
-void test_vector_init();
-
-void test_vector_push_back1();
-void test_vector_push_back2();
-
-void test_vector_get1();
-void test_vector_get2();
-
-void test_vector_set1();
-void test_vector_set2();
-
-void test_vector_find1();
-void test_vector_find2();
-void test_vector_find3();
-void test_vector_find4();
-
-void test_vector_delete_by_index1();
-void test_vector_delete_by_index2();
-void test_vector_delete_by_index3();
-void test_vector_delete_by_index4();
-
-void test_vector_delete_by_value1();
-void test_vector_delete_by_value2();
-void test_vector_delete_by_value3();
-void test_vector_delete_by_value4();
+#include "../include/vector.h"
+#include "test_vector.h"
 
 void test_delete(void *key) { *(int *)key = 0; }
 
---
+++ linear_data_structures/vector/tests/test_vector.h
@@ -0,0 +1,35 @@
+//
+// Created by dosart on 01.05.2021.
+//
+
+#ifndef LINEAR_DATA_STRUCTURES_USING_C_TEST_VECTOR_H
+#define LINEAR_DATA_STRUCTURES_USING_C_TEST_VECTOR_H
+
+void test_vector_init_zero_size();
+void test_vector_init();
+
+void test_vector_push_back1();
+void test_vector_push_back2();
+
+void test_vector_get1();
+void test_vector_get2();
+
+void test_vector_set1();
+void test_vector_set2();
+
+void test_vector_find1();
+void test_vector_find2();
+void test_vector_find3();
+void test_vector_find4();
+
+void test_vector_delete_by_index1();
+void test_vector_delete_by_index2();
+void test_vector_delete_by_index3();
+void test_vector_delete_by_index4();
+
+void test_vector_delete_by_value1();
+void test_vector_delete_by_value2();
+void test_vector_delete_by_value3();
+void test_vector_delete_by_value4();
+
+#endif //LINEAR_DATA_STRUCTURES_USING_C_TEST_VECTOR_H
