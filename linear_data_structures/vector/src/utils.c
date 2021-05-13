bf73d323cbc94f2e68b9054eda4ea8ef2ee45088
dosart <Dysprosium92@ya.ru>
2021-05-13 13:21:29 +0300

    Bug fix function vector_swap

--- linear_data_structures/vector/src/utils.c
+++ linear_data_structures/vector/src/utils.c
@@ -8,12 +8,16 @@ void *get_item(vector_t *v, size_t index) {
   return (char *)v->data + index * v->elem_size;
 }
 
+ void swap(void* a, void* b, size_t size) {
+    char tmp;
+    size_t i;
+    for (i = 0; i < size; i++) {
+        tmp = *((char*)b + i);
+        *((char*)b + i) = *((char*)a + i);
+        *((char*)a + i) = tmp;
+    }
+}
+
 void vector_swap(vector_t *v, size_t index1, size_t index2, size_t elem_size) {
-  char tmp;
-  for (size_t i = 0; i < elem_size; ++i) {
-    tmp = *(char *)((char *)v->data + index1 + i);
-    *(char *)((char *)v->data + index1 + i) =
-        *(char *)((char *)v->data + index2 + i);
-    *(char *)((char *)v->data + index1 + i) = tmp;
-  }
+  swap(vector_get(v, index1), vector_get(v, index2), elem_size);
 }
