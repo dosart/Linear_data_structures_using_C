119fa7e4ca5383b2d0ba647283d8760e717ec078
dosart <Dysprosium92@ya.ru>
2021-05-15 18:08:56 +0300

    Update sort.h

--- linear_data_structures/vector/include/sort.h
+++ linear_data_structures/vector/include/sort.h
@@ -28,6 +28,4 @@ void vector_bubble_sort(vector_t *v, int (*cmp)(void *, void *));
  */
 void vector_insertion_sort(vector_t *v, int (*cmp)(void *, void *));
 
-void vector_quick_sort(vector_t* v, int (*cmp)(void*, void*))
-
 #endif // LINEAR_DATA_STRUCTURES_USING_C_SORT_H
