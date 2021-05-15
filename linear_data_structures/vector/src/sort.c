372db78dd6bf81eab0632099351d45cc8626cae7
dosart <Dysprosium92@ya.ru>
2021-05-15 18:06:57 +0300

    Update sort.c

--- linear_data_structures/vector/src/sort.c
+++ linear_data_structures/vector/src/sort.c
@@ -26,30 +26,3 @@ void vector_insertion_sort(vector_t *v, int (*cmp)(void *, void *)) {
     }
   }
 }
-
-size_t vector_partition(vector_t* v, size_t start, size_t end, int (*cmp)(void*, void*)) {
-    void* pivot = vector_get(v, end);
-    size_t pivot_idx = start;
-
-    for (size_t i = start; i < end; ++i) {
-        void* current = vector_get(v, i);
-        if (cmp(current, pivot)) {
-            vector_swap(v, i, pivot_idx, v->elem_size);
-            pivot_idx++;
-        }
-    }
-    vector_swap(v, end, pivot_idx, v->elem_size);
-    return pivot_idx;
-}
-
-static  void quick_sort(vector_t* v, size_t start, size_t end,  int (*cmp)(void*, void*)) {
-     if (start < end) {
-         size_t pivot_idx = vector_partition(v, start, end, cmp);
-         quick_sort(v, start, pivot_idx - 1, cmp);
-         quick_sort(v, pivot_idx + 1, end, cmp);
-     }
- }
-
-void vector_quick_sort(vector_t* v, int (*cmp)(void*, void*)) {
-    quick_sort(v, 0, v->size - 1, cmp);
-}
