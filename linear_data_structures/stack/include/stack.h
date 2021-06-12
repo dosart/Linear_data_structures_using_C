28621aa0cb10318d901103d336db74a38f1e1acd
Denis <Dysprosium92@ya.ru>
2021-06-12 17:50:23 +0300

    Added stack

--- linear_data_structures/stack/include/stack.h
+++ linear_data_structures/stack/include/stack.h
@@ -1,3 +1,10 @@
+//
+// Created by dosart on 6/12/21.
+//
+
+#ifndef UNTITLED1__STACK_H_
+#define UNTITLED1__STACK_H_
+
 /*!
 @file
 @brief  Header file for stack
@@ -7,12 +14,13 @@ This file contains the definition of the data structure stack
 #ifndef LINEAR_DATA_STRUCTURES_USING_C_STACK_H
 #define LINEAR_DATA_STRUCTURES_USING_C_STACK_H
 
-#include <mem.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+#include "assert.h"
 
 /**
- * \defgroup stack Stack data structure
+ * \defgroup stack
  *
  * @brief Item of stack
  *
@@ -26,7 +34,7 @@ This file contains the definition of the data structure stack
  */
 typedef struct stack_item_t {
   void *data;
-  struct list_item_t *next;
+  struct stack_item_t *next;
 } stack_item_t;
 
 /**
@@ -43,60 +51,87 @@ typedef struct stack_item_t {
  * @warning This structure created only for educational goals
  */
 typedef struct stack_t {
-  stack_item_t *first, last;
-  size_t elem_size;
-  size_t size;
+  stack_item_t *first, *last;
+  size_t elem_size; ///< size of one element
+  size_t size; ///< count of elements on the stack
 } stack_t;
 
 /**
  * @ingroup stack
  *
- * @brief Construct new stack data structure
+ * @brief Construct new stack data structure.
  *
- * @param elem_size Size of stack item
+ * @param s Pointer to stack data structure.
+ * @param elem_size Size of stack item.
  */
 void stack_init(stack_t *v, size_t elem_size);
 
 /**
  * @ingroup stack
  *
- * @brief Returns true if stack is empty (expressed in terms of elements)
+ * @brief Returns true if stack is empty (expressed in terms of elements).
  *
- * @param v pointer to stack data structure
+ * @param s Pointer to stack data structure.
  *
- * @return 1 if size == 0 else 0
+ * @return 1 if size == 0 else 0.
  */
 size_t stack_is_empty(stack_t *s);
 
 /**
  * @ingroup stack
  *
- * @brief Returns size of stack (expressed in terms of elements)
+ * @brief Returns size of stack (expressed in terms of elements).
  *
- * @param v pointer to stack data structure
+ * @param s Pointer to stack data structure.
  *
- * @return size of stack
+ * @return Size of stack.
  */
 size_t stack_size(stack_t *s);
 
 /**
  * @ingroup stack
  *
- * @brief Free memory in stack_item_t
+ * @brief Free memory in stack_item_t.
  *
- * @param v pointer to stack
- * @param deleter function to remove an item
+ * @param s Pointer to stack.
+ * @param deleter Deleter function to remove an item.
  */
 void stack_free(stack_t *s, void (*deleter)(void *));
 
 /**
  * @ingroup stack
  *
- * @brief Add item to stack
+ * @brief Add item to stack.
  *
- * @param v pointer to stack
- * @param item element for add
+ * @param s pointer to stack.
+ * @param item element for add.
  */
 void stack_push(stack_t *s, void *item);
 
+/**
+ * @ingroup stack
+ *
+ * @brief Looks at the object at the top of this s without removing it from the s.
+ *
+ * @param s Pointer to s.
+ *
+ * @return Top of this s.
+ */
+void* stack_peek(stack_t *s);
+
+/**
+ * @ingroup stack
+ *
+ * @brief Removes the object at the top of this stack and returns that object as the value of this function.
+ *
+ * @param s Pointer to s.
+ *
+ * @return Top of this s.
+ * @param deleter Deleter function to remove an item.
+ */
+void stack_pop(stack_t *s, void (*deleter)(void *));
+
 #endif // LINEAR_DATA_STRUCTURES_USING_C_STACK_H
+
+#endif //UNTITLED1__STACK_H_
+
--- linear_data_structures/stack/src/stack.c
+++ linear_data_structures/stack/src/stack.c
@@ -1,7 +1,8 @@
-#include "../include/stack.h"
+
+#include "stack.h"
 
 void stack_init(stack_t *s, size_t elem_size) {
-  if (s != NULL) {
+  if (s!=NULL) {
     s->elem_size = elem_size;
     s->first = s->last = NULL;
     s->size = 0;
@@ -9,50 +10,84 @@ void stack_init(stack_t *s, size_t elem_size) {
 }
 
 size_t stack_is_empty(stack_t *s) {
-  if (s != NULL) {
-    return s->size == 0 ? 1 : 0;
+  if (s!=NULL) {
+    return s->size==0 ? 1 : 0;
   }
   return 0;
 }
 
 size_t stack_size(stack_t *s) {
-  if (s != NULL) {
+  if (s!=NULL) {
     return s->size;
   }
   return 0;
 }
 
+void *stack_peek(stack_t *s) {
+  if (s!=NULL && s->size > 0) {
+    return s->first->data;
+  }
+  return NULL;
+}
+
 void stack_free(stack_t *s, void (*deleter)(void *)) {
-  if (s != NULL) {
+  if (s!=NULL) {
     stack_item_t *cur = s->first;
     stack_item_t *tmp = NULL;
     while (cur) {
       deleter(cur->data);
       tmp = cur->next;
+      free(cur->data);
       free(cur);
       cur = tmp;
     }
   }
 }
 
-static void copy(void *destination, void *source, size_t size) {
-  memcpy((char *)destination, (char *)source, size);
+void stack_pop(stack_t *s, void (*deleter)(void *)) {
+  if (s && deleter) {
+    stack_item_t* head = s->first;
+    if (head) {
+      deleter(head->data);
+      free(head->data);
+      if (head->next)
+        s->first = head->next;
+      free(head);
+      if (--s->size)
+        s->last = NULL;
+      else
+        s->first = s->last = NULL;
+    }
+  }
 }
 
 static stack_item_t *stack_make_item(void *value, size_t value_size,
-                                     stack_item_t *next) {
-  stack_item_t *new_item = malloc(sizeof(stack_item_t) + sizeof(v->elem_size));
+                                     stack_item_t *next);
 
-  new_item->data = new_item + sizeof(stack_item_t);
+void stack_push(stack_t *s, void *item) {
+  if (s!=NULL) {
+    s->first = stack_make_item(item, s->elem_size, s->first);
+    if (s->last==NULL)
+      s->last = s->first;
+    s->size++;
+  }
+}
+
+static void copy(void *destination, void *source, size_t size);
+
+static stack_item_t *stack_make_item(void *value, size_t value_size,
+                                     stack_item_t *next) {
+  stack_item_t *new_item = malloc(sizeof(stack_item_t));
+  new_item->next = next;
+
+  new_item->data = malloc(sizeof(value_size));
   copy(new_item->data, value, value_size);
 
-  new_item->next = next;
   return new_item;
 }
 
-void stack_push(stack_t *s, void *item) {
-  if (s != NULL) {
-    s->first = stack_make_item(item, s->elem_size, s->first);
-    if (s->last == NULL)
-  }
+static void copy(void *destination, void *source, size_t size) {
+  memcpy((char *) destination, (char *) source, size);
 }
+
+
